7d1989d64b03e347ae1ddf4aec9d5167fde0e32e
camilo <camilofetecua2011@outlook.com>
2024-01-22 14:30:25 -0500

    add betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,18 @@
 #include "binary_trees.h"
 
-
-
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
+ *
+ * Description:
+ * - Allocates memory for a new binary tree node and initializes its values.
+ *
+ * @parent: Parent node of the new node.
+ * @value: Integer value to be stored in the new node.
+ *
+ * Return: Pointer to the new node, or NULL on failure.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
