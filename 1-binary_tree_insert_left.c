962a4c9f9e872d5a2eb9d2395b8097fc9bc228e1
camilo <camilofetecua2011@outlook.com>
2024-01-23 08:16:29 -0500

    Remove the comment from line 29 to fix Betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	/* Check if parent is NULL */
 	if (parent == NULL)
 		return (NULL);
@@ -25,8 +25,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	
-	/* If parent already has a left child, replace it and set old left as left of the new node */
+
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
